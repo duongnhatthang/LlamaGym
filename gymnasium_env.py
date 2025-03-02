d85582b2b5f61419a981c97306d275571eafbd2f
Thang PC home <duongnhatthang@gmail.com>
2025-03-02 08:55:15 -0700

    merge with d3rlpy. Tested successfully Off and Online RL in gymnasium_env.py

---
+++ gymnasium_env.py
@@ -0,0 +1,64 @@
+import argparse
+import gymnasium
+import d3rlpy
+import numpy as np
+
+class BlackjackWrapper(gymnasium.ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = gymnasium.spaces.Box(low=-1, high=32, shape=(3,), dtype=np.int32)
+
+    def observation(self, obs):
+        return np.array(obs, dtype=np.int32)
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser()
+    # parser.add_argument("--env", type=str, default="Hopper-v2")
+    parser.add_argument("--env", type=str, default="Blackjack-v1")
+    parser.add_argument("--seed", type=int, default=1)
+    parser.add_argument("--gpu", action="store_true")
+    args = parser.parse_args()
+
+    # d3rlpy supports both Gym and Gymnasium
+    if args.env=="Blackjack-v1":
+        env = BlackjackWrapper(gymnasium.make("Blackjack-v1"))
+        eval_env = BlackjackWrapper(gymnasium.make("Blackjack-v1"))
+    else:
+        env = gymnasium.make(args.env)
+        eval_env = gymnasium.make(args.env)
+
+    # fix seed
+    d3rlpy.seed(args.seed)
+    d3rlpy.envs.seed_env(env, args.seed)
+    d3rlpy.envs.seed_env(eval_env, args.seed)
+
+    # setup algorithm
+    dqn = d3rlpy.algos.DQNConfig(
+        batch_size=32,
+        learning_rate=6.25e-5,
+        ).create(device=args.gpu)
+
+    with open('llm_data.pkl', 'rb') as file:
+        dataset = pickle.load(file)
+
+    # start offline training
+    dqn.fit(dataset, n_steps=100000)
+
+    # replay buffer for experience replay
+    buffer = d3rlpy.dataset.create_fifo_replay_buffer(limit=1000000, env=env)
+
+    # start training
+    dqn.fit_online(
+        env,
+        buffer,
+        eval_env=eval_env,
+        n_steps=1000000,
+        n_steps_per_epoch=10000,
+        update_interval=1,
+        update_start_step=1000,
+    )
+
+
+if __name__ == "__main__":
+    main()
