e55a6fb65843c14e83f2b6410d08a0776f54831e
Thang PC home <duongnhatthang@gmail.com>
2025-03-26 17:19:48 -0700

    delete files

--- env/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from .atari import register_environments
-from .atari import SpaceInvaders_translator
-
-register_environments()
-
-# REGISTRY = {}
-# REGISTRY["RepresentedSpaceInvaders_init_translator"] = SpaceInvaders_translator.GameDescriber
-# REGISTRY["RepresentedSpaceInvaders_basic_translator"] = SpaceInvaders_translator.TransitionTranslator
