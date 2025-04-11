ac719bd46f47c7ec4ae0c75838bdacd5183a18e2
Thang Macbook <duongnhatthang@gmail.com>
2025-04-10 22:26:25 -0700

    Fix minor bug

--- env/toy_text/frozenlake_translator.py
+++ env/toy_text/frozenlake_translator.py
@@ -67,7 +67,7 @@ class GameDescriber:
                 "\n '3': Move right, which means ('x', 'y+1')," \
                 "\n '4': Move up, which means trying to step into ('x-1', 'y')." \
                 " Ensure you only provide the action number from the valid action list, i.e., [1, 2, 3, 4]. " \
-               "Do not return the target's coordinator.")
+               "Do not return the target's coordination.")
 
 class TransitionTranslator(ObsTranslator):
     def translate(self, infos, is_current=False):
