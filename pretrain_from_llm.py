600834ef1b04649cd60f7f05c0ff5626ed24fff4
Thang Macbook <duongnhatthang@gmail.com>
2025-04-11 11:09:27 -0700

    Fix minor bug

--- pretrain_from_llm.py
+++ pretrain_from_llm.py
@@ -45,10 +45,12 @@ d3rlpy.seed(hyperparams["seed"])
 d3rlpy.envs.seed_env(env, hyperparams["seed"])
 d3rlpy.envs.seed_env(eval_env, hyperparams["seed"])
 
-with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-32B-Instruct_Neps_30_20250409124533.pkl", 'rb') as file:
+with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-32B-Instruct_Neps_30_20250411030422.pkl", 'rb') as file: #FrozenLake
+# with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-32B-Instruct_Neps_30_20250409124533.pkl", 'rb') as file: #CartPole with Eps
     Qwen_32B_dataset = pickle.load(file)
 
-with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-7B-Instruct_Neps_30_20250409023954.pkl", 'rb') as file:
+with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-7B-Instruct_Neps_30_20250411000858.pkl", 'rb') as file: #FrozenLake
+# with open(f"data/{hyperparams['env'].split('-')[0]}_Qwen2.5-7B-Instruct_Neps_30_20250409023954.pkl", 'rb') as file: #CartPole with Eps
     Qwen_7B_dataset = pickle.load(file)
 
 def get_new_dataset(dataset, n_eps):
