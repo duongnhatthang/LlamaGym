f7636412c1796e0446afbda8eed024b69cf49ae9
Thang Macbook <duongnhatthang@gmail.com>
2025-04-12 15:15:19 -0700

    minor change

--- llm_main.py
+++ llm_main.py
@@ -273,7 +273,7 @@ if __name__ == "__main__":
                 action = env.action_space.sample()
             else:
                 action = agent.act(observation)
-                print(agent.current_episode_messages)
+                # print(agent.current_episode_messages)
             # wandb.log({"action": action})
             observation, reward, done, info = env.step(action)
             if "Cliff" in hyperparams["env"] or "Frozen" in hyperparams["env"]:
