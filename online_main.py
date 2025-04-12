460e7d4fe3cdfe850ee0e79a6b76eaa3a8131cb4
Thang Macbook <duongnhatthang@gmail.com>
2025-04-11 23:34:46 -0700

    test mountain car

--- online_main.py
+++ online_main.py
@@ -100,14 +100,14 @@ def online_training(
 
 if __name__ == "__main__":
     hyperparams = {
-        "env": "FrozenLake-v1", #"CartPole-v0", # "Acrobot-v0", "MountainCar-v0", "FrozenLake-v1", "CliffWalking-v0", "Taxi-v3", "RepresentedPong-v0"
+        "env": "MountainCar-v0", #"CartPole-v0", # "Acrobot-v0", "MountainCar-v0", "FrozenLake-v1", "CliffWalking-v0", "Taxi-v3", "RepresentedPong-v0"
         "seed": 42069,
         "n_episodes": 200,#5000,
         "max_episode_len": 200, # Around 10h per 100k steps in Leviathan server
         "eps": 0.1,  # epsilon for exploration
         "n_exp": 5,
-        "n_pretrain_eps": 10,
-        "n_online_eps": 290, #10-290 for mountainCar, 30-170 for CartPole, 30-270 for FrozenLake
+        "n_pretrain_eps": 30,
+        "n_online_eps": 270, #10-290 for mountainCar, 30-170 for CartPole, 30-270 for FrozenLake
         "gpu": True, # True if use GPU to train with d3rlpy
         "buffer_size": 100000, #Test with 100k, 200k, 500k. 1M might be too much
         "data_path": None,#'data/CartPole_Qwen2.5-7B-Instruct_Neps_10_20250406040150.pkl',
