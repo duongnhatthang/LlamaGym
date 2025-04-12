4fc7e74dce5fd172952db89c0623c987b8d158ca
Thang Macbook <duongnhatthang@gmail.com>
2025-04-12 06:52:19 -0700

    pretrain and online cartpole again

--- pretrain_from_llm.py
+++ pretrain_from_llm.py
@@ -7,14 +7,14 @@ import pandas as pd
 from env.atari.represented_atari_game import GymCompatWrapper2
 
 hyperparams = {
-        "env": "FrozenLake-v1", #"CartPole-v0", # "Acrobot-v0", "MountainCar-v0", "FrozenLake-v1", "CliffWalking-v0", "Taxi-v3", "RepresentedPong-v0"
+        "env": "CartPole-v0", #"CartPole-v0", # "Acrobot-v0", "MountainCar-v0", "FrozenLake-v1", "CliffWalking-v0", "Taxi-v3", "RepresentedPong-v0"
         "seed": 42069,
         "n_episodes": 10,#5000,
         "max_episode_len": 200, # Around 10h per 100k steps in Leviathan server
         "eps": 0.1,  # epsilon for exploration
         "n_exp": 5,
-        "n_pretrain_eps": 30,
-        "n_online_eps": 270,
+        "n_pretrain_eps": 10,
+        "n_online_eps": 1400,
         "gpu": True, # True if use GPU to train with d3rlpy
         "buffer_size": 100000, #Test with 100k, 200k, 500k. 1M might be too much
         "data_path": None,#'data/CartPole_Qwen2.5-7B-Instruct_Neps_10_20250406040150.pkl',
@@ -24,7 +24,7 @@ hyperparams = {
         "gamma":0.99,
         "target_update_interval":1000, #Test with 1k, 2k, 5k
         "n_steps_per_epoch": 200,
-        "n_pretrain_steps": 3000
+        "n_pretrain_steps": 1000
     }
 
 n_pretrain_eps = hyperparams["n_pretrain_eps"] #10
